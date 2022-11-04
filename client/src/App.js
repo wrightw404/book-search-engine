a10e17633344b2569b566be2995a19d686af7520
Will <wrightw404@gmail.com>
2022-11-04 16:02:10 -0700

    started working on ApolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+//set up apollo
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  }
+})
 
 function App() {
   return (
