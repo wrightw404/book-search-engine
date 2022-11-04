13b83e8fa3be61752c82ea0bd826a3ed730feba0
Will <wrightw404@gmail.com>
2022-11-04 15:48:35 -0700

    created mutations.js file and defined mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,75 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+    mutation loginUser(email: String!, password: String!){
+        login(email: $email, password: $password){
+            token
+            user{
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser(username: String!, password: String!){
+        login(username: $username, password: $password){
+            token
+            user{
+                _id
+                username
+                email
+                bookCount
+                savedBooks{
+                    #_id
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input:savedBook!) {
+        saveBook(input: $input){
+                _id
+                username
+                email
+                bookCount
+                savedBooks{
+                    #_id
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId:ID!) {
+        removeBook(bookId: $bookId){
+                _id
+                username
+                email
+                bookCount
+                savedBooks{
+                    #_id
+                    bookId
+                    authors
+                    description
+                    title
+                    image
+                    link
+                }
+        }
+    }
+`;
