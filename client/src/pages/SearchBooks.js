96c35eba181a3eed8d31a7a12ba32283609c6d37
Will <wrightw404@gmail.com>
2022-11-04 18:40:26 -0700

    delete seeds folder

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -19,6 +19,7 @@ const SearchBooks = () => {
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
   const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
