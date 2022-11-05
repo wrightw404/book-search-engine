88e0863ebc2be74512d16510f9c6c4de7767a091
Will <wrightw404@gmail.com>
2022-11-04 18:22:49 -0700

    went through problem solving the errors when trying to build

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const LOGIN_USER = gql`
-    mutation loginUser(email: String!, password: String!){
+    mutation loginUser($email: String!, $password: String!){
         login(email: $email, password: $password){
             token
             user{
@@ -12,8 +12,8 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-    mutation addUser(username: String!, password: String!){
-        login(username: $username, password: $password){
+    mutation addUser($username: String!, $password: String!, $email: String!){
+        login(username: $username, password: $password, email: $email){
             token
             user{
                 _id
